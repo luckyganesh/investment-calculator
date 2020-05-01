6d2ce6ce883526b0b3ba11f41d54712636c121f7
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2020-05-01 18:38:16 +0530

    Add logic for SMA.

--- src/investment_calculator/core.clj
+++ src/investment_calculator/core.clj
@@ -1,20 +1,7 @@
 (ns investment-calculator.core
-  (:require [investment-calculator.logic-sip :as logic-sip]))
-
-
-(def my-values [{:date "2020-03-12", :nav 318.3876}
-                {:date "2020-03-13", :nav 318.4047}
-                {:date "2020-03-14", :nav 318.452}
-                {:date "2020-03-15", :nav 318.4986}
-                {:date "2020-03-17", :nav 318.5649}
-                {:date "2020-03-18", :nav 318.5465}
-                {:date "2020-03-19", :nav 318.3051}
-                {:date "2020-03-21", :nav 318.3932}
-                {:date "2020-03-22", :nav 318.4497}
-                {:date "2020-03-23", :nav 318.2253}
-                {:date "2020-03-26", :nav 318.2818}
-                {:date "2020-03-27", :nav 319.1901}
-                {:date "2020-03-28", :nav 319.2373}])
+  (:require [investment-calculator.logic-sip :as logic-sip]
+            [investment-calculator.logic-SMA :as logic-sma]))
 
 (defn -main [& args]
-  (logic-sip/make-sip))
+  (let [config (logic-sip/json-to-clojure-data-converter "config.json")]
+    (logic-sma/make-SMA config)))
---
+++ src/investment_calculator/logic_SMA.clj
@@ -0,0 +1,72 @@
+(ns investment-calculator.logic-SMA
+  (:require [investment-calculator.utils :as u]
+            [investment-calculator.logic-sip :as logic-sip]
+            [clojure.string :as string]))
+
+(defn read-parse-fund [fund]
+  (->> fund
+       (logic-sip/create-sip-records)
+       (string/trim)
+       (logic-sip/create-map-from-csv)
+       (map #(update-in % [:nav] logic-sip/parse-double))
+       (u/stabilize-dates)))
+
+(defn merge-navs [{date :date nav :nav} {s-nav :nav}]
+  {
+   :date  date
+   :nav   nav
+   :s-nav s-nav})
+
+(defn calculate-smas [sma navs]
+  (->> navs
+       (reverse)
+       (partition-all sma 1)
+       (map #(/ (apply + %) (count %)))
+       (reverse)))
+
+(defn add-smas [sma-days records]
+  (let [navs (map :nav records)
+        smas (calculate-smas sma-days navs)]
+    (map #(assoc %1 :sma %2) records smas)))
+
+; nu - new units of main fund
+; tu - total units of main fund
+; nau - new units of alternative fund
+; tau - total units of alternative fund
+
+
+(defn new-units-decision [amount date nav]
+  (if (= (subs date 8) "01")
+    (/ amount nav)
+    0))
+
+(defn add-new-units [amount {ptu :tu ptau :tau actual-worth :actual-worth} {date :date nav :nav s-nav :s-nav sma :sma}]
+  (let [nub (new-units-decision amount date nav)
+        naub (new-units-decision amount date s-nav)
+        nu (+ nub (/ (* ptau s-nav) nav))
+        nau (+ naub (/ (* ptu nav) s-nav))
+        worth (+ actual-worth (if (= (subs date 8) "01") amount 0))
+        current-record {:date date :nav nav :s-nav s-nav :sma sma :actual-worth worth}]
+    (if (< nav sma)
+      (-> current-record
+          (assoc :nu (- ptu))
+          (assoc :tu 0)
+          (assoc :nau nau)
+          (assoc :tau (+ nau ptau)))
+      (-> current-record
+          (assoc :nu nu)
+          (assoc :tu (+ nu ptu))
+          (assoc :nau (- ptau))
+          (assoc :tau 0)))))
+
+(defn add-total-worth [record]
+  (assoc record :total-worth (+ (* (:nav record) (:tu record)) (* (:s-nav record) (:tau record)))))
+
+(defn make-SMA [config]
+  (let [fund-records (read-parse-fund (:fund config))
+        alternative-fund-records (read-parse-fund (:alternative-fund config))
+        merged-funds (map merge-navs fund-records alternative-fund-records)
+        added-smas (add-smas (:sma config) merged-funds)
+        new-units (rest (reductions (partial add-new-units (:amount config)) {:tu 0, :tau 0 :actual-worth 0} added-smas))
+        total-worth (map add-total-worth new-units)]
+    (count (map println total-worth))))
